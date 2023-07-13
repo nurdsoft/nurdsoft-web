516a70e8021528578c8bfbd222b1dbdc74a2eb44
rogdevil <sep2001aryan@yahoo.com>
2023-07-13 17:10:52 +0530

    bugs

--- src/components/layouts/aboutlayout.tsx
+++ src/components/layouts/aboutlayout.tsx
@@ -1,12 +1,75 @@
 import * as React from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Contact from "../contact/contact";
+import AboutPage4 from "../about/aboutPage4";
+import AboutPage2 from "../about/aboutPage2";
+import AboutPage1 from "../about/aboutPage1";
 
 const AboutLayout = () => {
   return (
     <Parallax pages={2}>
       <ParallaxLayer
-        offset={7}
+        offset={0}
+        speed={1}
+        factor={0}
+        style={{
+          backgroundColor: "var(--black-100)",
+          height: "100vh",
+          paddingBottom: "40px",
+        }}
+      >
+        <AboutPage1 />
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={1}
+        speed={1}
+        factor={0}
+        style={{
+          backgroundColor: "var(--black-100)",
+          height: "100vh",
+          paddingBottom: "40px",
+        }}
+      >
+        <AboutPage2 />
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={2}
+        speed={1}
+        factor={0}
+        style={{
+          backgroundColor: "var(--black-100)",
+          height: "100vh",
+          paddingBottom: "40px",
+        }}
+      >
+        <p>Team Map section</p>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={3}
+        speed={1}
+        factor={0}
+        style={{
+          backgroundColor: "var(--black-100)",
+          height: "100vh",
+          paddingBottom: "40px",
+        }}
+      >
+        <AboutPage4 />
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={4}
+        speed={1}
+        factor={0}
+        style={{
+          backgroundColor: "var(--black-100)",
+          height: "100vh",
+          paddingBottom: "40px",
+        }}
+      >
+        <p>Hire now section</p>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={5}
         speed={1}
         factor={0}
         style={{
