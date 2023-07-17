eff5e7ecd0d8aab1d0c780546e00abdf7fbd8583
rogdevil <sep2001aryan@yahoo.com>
2023-07-17 17:31:09 +0530

    added section 1 and section 2 of career page

---
+++ src/components/career/careerPage2.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Wrapper from '../wrapper';
+import './careerPage2.scss';
+import { CAREER_CLOCK_ICON, CAREER_HEART_ICON, CAREER_HOME_ICON } from '../../icons';
+
+const CareerPage2 = () => {
+  const statsData = [
+    {
+      icon: <CAREER_HOME_ICON className='careerPage2_Icon' />,
+      label: "Work From Anywhere"
+    },
+    {
+      icon: <CAREER_CLOCK_ICON className='careerPage2_Icon' />,
+      label: "Flexible Hours"
+    },
+    {
+      icon: <CAREER_HEART_ICON className='careerPage2_Icon' />,
+      label: "Health Insurance"
+    }
+  ]
+  return (
+    <Wrapper>
+      <div className="careerPage2_parentContainer">
+        <div className='companyPerks'>
+          {
+            statsData.map((data, index) => (
+              <div className='perk' key={index}>
+                <p className='perkIcon'>{data.icon}</p>
+                <p className='perkLabel'>{data.label}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default CareerPage2
