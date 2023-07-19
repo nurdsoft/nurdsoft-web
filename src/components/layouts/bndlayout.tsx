4769507f7c7a67f83de6656115d873829373d7bb
Prakhar Dixit <prakhar.dixit@nurdsoft.co>
2023-07-19 21:00:55 +0530

    fix: merge with aryan branch

--- src/components/layouts/bndlayout.tsx
+++ src/components/layouts/bndlayout.tsx
@@ -10,11 +10,12 @@ const BndLayout = () => {
         <ParallaxLayer
           offset={0}
           speed={0}
-          factor={0}
+          factor={1}
           style={{
             backgroundColor: "var(--black-100)",
             height: "100%",
             overflow: "scroll",
+            paddingBottom: "70px",
           }}
         >
           <BreadCrumb title="BnD" />
