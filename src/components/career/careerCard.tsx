b945317eafc24752fd647e03414f79d1505d3ecc
rogdevil <sep2001aryan@yahoo.com>
2023-07-20 17:15:06 +0530

    structural changes related to job Icons

--- src/components/career/careerCard.tsx
+++ src/components/career/careerCard.tsx
@@ -1,64 +1,69 @@
 import React from 'react';
 import Button from '../button';
-import { BUSINESS_ANALYST_ICON, DATA_SCIENCE_ENGINEER_ICON, DEVOPS_ENGINEER_ICON, DIGITAL_MARKETING_ICON, HUMAN_RESOURCE_ICON, LEAD_SOFTWARE_ENGINEER_ICON, PROJECT_MANAGER_ICON, SALESFORCE_ICON, SOFTWARE_ENGINEER_TESTING_ICON, SR_MOBILE_ENGINEER_ICON, SR_SOFTWARE_ENGINEER_BE_ICON, SR_SOFTWARE_ENGINEER_FE_ICON, SR_SOFTWARE_ENGINEER_ML_ICON, TECH_PROJECT_MANAGER_ICON, UI_UX_DESIGNER_ICON } from '../../icons';
+import { iconMap } from '../../jobData';
 
 const CareerCard = () => {
+
   const jobData = [
     {
       position: 'Lead Software Engineer',
-      icon: <LEAD_SOFTWARE_ENGINEER_ICON className='careerCardIcon' />
+      icon: iconMap['softwareEngineer']
     },
     {
       position: 'Sr. Software Engineer, Front-end',
-      icon: <SR_SOFTWARE_ENGINEER_FE_ICON className='careerCardIcon' />
+      icon: iconMap['frontendEngineer']
     },
     {
       position: 'Sr. Software Engineer, Back-end',
-      icon: <SR_SOFTWARE_ENGINEER_BE_ICON className='careerCardIcon' />
+      icon: iconMap['backendEngineer']
     },
     {
       position: 'Sr. Software Engineer, Machine Learning',
-      icon: <SR_SOFTWARE_ENGINEER_ML_ICON className='careerCardIcon' />
+      icon: iconMap['machineLearningEngineer']
     },
     {
       position: 'Sr. Software Engineer, Mobile',
-      icon: <SR_MOBILE_ENGINEER_ICON className='careerCardIcon' />
+      icon: iconMap['mobileEngineer']
     },
     {
       position: 'DevOps Engineer',
-      icon: <DEVOPS_ENGINEER_ICON className='careerCardIcon' />
+      icon: iconMap['devopsEngineer']
     },
     {
       position: 'Software Engineer in Testing',
-      icon: <SOFTWARE_ENGINEER_TESTING_ICON className='careerCardIcon' />
+      icon: iconMap['testingEngineer']
     },
     {
       position: 'Technical Project Manager',
-      icon: <TECH_PROJECT_MANAGER_ICON className='careerCardIcon' />
+      icon: iconMap['projectManager']
     },
     {
       position: 'Digital Marketing & SEO Specialist',
-      icon: <DIGITAL_MARKETING_ICON className='careerCardIcon' />
+      icon: iconMap['digitalMarketing']
     },
     {
       position: 'Salesforce Developer',
-      icon: <SALESFORCE_ICON className='careerCardIcon' />
+      icon: iconMap['salesforceEngineer']
     },
     {
       position: 'Head of Human Resources and Recruitment',
-      icon: <HUMAN_RESOURCE_ICON className='careerCardIcon' />
+      icon: iconMap['humanResource']
     },
     {
       position: 'Data Science Engineer',
-      icon: <DATA_SCIENCE_ENGINEER_ICON className='careerCardIcon' />
+      icon: iconMap['dataScientist']
     },
     {
       position: 'Business Analyst',
-      icon: <BUSINESS_ANALYST_ICON className='careerCardIcon' />
+      icon: iconMap['businessAnalyst']
     },
     {
       position: 'Product Manager',
-      icon: <PROJECT_MANAGER_ICON className='careerCardIcon' />
+      icon: iconMap['productManager']
+    },
+    {
+      position: 'UI/UX Designer',
+      icon: iconMap['uiuxDesigner']
     },
   ]
 
