b77c06b3180bfc4a083cdbee2346092cac7e3415
rogdevil <sep2001aryan@yahoo.com>
2023-07-28 12:26:59 +0530

    section 3 ready service section

--- src/components/services/servicespage3.tsx
+++ src/components/services/servicespage3.tsx
@@ -1,9 +1,27 @@
-import * as React from "react";
+import React, {useEffect, useRef} from "react";
 import "./servicespage3.scss";
+import { animated, useSpring } from '@react-spring/web';
 import Wrapper from "../wrapper";
 import Button from "../button";
+import useIntersectionObserver from "../common/intersectionObserver";
 
 const ServicesPage3 = () => {
+  const triggerRef = useRef<any>();
+
+  const dataRef = useIntersectionObserver(triggerRef, {
+    threshold: 0.4,
+    freezeOnceVisible: false
+  });
+
+  const headerStyle = useSpring({
+    config: { duration: 500 },
+    from: { transform: 'translateY(200px)', opacity: 0 },
+    to: {
+      transform: dataRef?.isIntersecting ? 'translateY(0px)' : 'translateY(200px)',
+      opacity: dataRef?.isIntersecting ? 1 : 0,
+    },
+  });
+
   const options = [
     "Flexible work terms",
     "Hungry and Unstoppable",
@@ -11,36 +29,42 @@ const ServicesPage3 = () => {
     "True partnership",
   ];
 
+  useEffect(() => {
+    console.log("inter", dataRef?.intersectionRatio)
+  }, [dataRef?.intersectionRatio])
+
   return (
     <Wrapper>
-      <div className="servicespage3_parentContainer" id="servicespage3_parentContainer">
+      <div className="servicespage3_parentContainer" id="servicespage3_parentContainer" ref={triggerRef}>
         <div className="servicespage3_gridContainer">
-          <div className="w-5/12 mt-12">
+          <div className="servicespage3_leftContainer mt-12">
             <div className="servicespage3_titleDiv lineUp">
-              Why to Work with Us
+              Why to Work <br/> with Us
             </div>
             <div className="mt-12">
               <Button buttonText="SCHEDULE CONSULTATION" />
             </div>
           </div>
           <div className="servicespage3_rightContainer">
-            <div className="servicespage3_listContainer mt-20 ml-12">
-              {options.map((option: string, index: number) => {
-                return (
-                  <div key={index} className={index ^ 0 ? "mt-16" : ""}>
-                    <small className="servicespage3_count ml-14">{`0${
-                      index + 1
-                    }`}</small>
-                    <div className="servicespage3_options flex items-center">
-                      <span className="mr-4 w-10">
-                        <hr className="servicespage3_line" />
-                      </span>
-                      <div className="servicespage3_optionsList">{option}</div>
+            <animated.div style={headerStyle}>
+              <div className="servicespage3_listContainer ml-12">
+                {options.map((option: string, index: number) => {
+                  return (
+                    <div key={index} className={index ^ 0 ? "mt-20" : ""}>
+                      <small className="servicespage3_count ml-14">{`0${
+                        index + 1
+                      }`}</small>
+                      <div className="servicespage3_options flex items-center">
+                        <span className="mr-4 w-10">
+                          <hr className="servicespage3_line" />
+                        </span>
+                        <div className="servicespage3_optionsList">{option}</div>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
-            </div>
+                  );
+                })}
+              </div>
+            </animated.div>
           </div>
         </div>
       </div>
