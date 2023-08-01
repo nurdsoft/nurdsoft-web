c03b9236217eb59a3da7db887181a1377055e5c6
rogdevil <sep2001aryan@yahoo.com>
2023-08-01 13:22:26 +0530

    bit fleq page ready and responsive

--- src/components/layouts/bitfleqslayout.tsx
+++ src/components/layouts/bitfleqslayout.tsx
@@ -1,28 +1,14 @@
-import * as React from "react";
+import React from "react";
 import BreadCrumb from "../breadcrumb";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import BitFleqpage from "../work/bitfleq/bitfleqpage";
+import './bitfleqslayout.scss';
 
 const BitFleqsLayout = () => {
   return (
-    <>
-      <Parallax pages={2}>
-        <ParallaxLayer
-          offset={0}
-          speed={0}
-          factor={1}
-          style={{
-            backgroundColor: "var(--black-100)",
-            height: "100%",
-            overflow: "scroll",
-            paddingBottom: "50px",
-          }}
-        >
-          <BreadCrumb title="BITFLEQS" />
-          <BitFleqpage />
-        </ParallaxLayer>
-      </Parallax>
-    </>
+    <div className="bitfleq_scroller">
+      <BreadCrumb title="BITFLEQS" />
+      <BitFleqpage />
+    </div>
   );
 };
 
