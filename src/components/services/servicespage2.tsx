f1836d225118c71353599767ea6b0d4ad3455640
rogdevil <sep2001aryan@yahoo.com>
2023-08-07 17:46:58 +0530

    Beta version ready for testing

--- src/components/services/servicespage2.tsx
+++ src/components/services/servicespage2.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useRef} from "react";
 import "./servicespage2.scss";
-import Wrapper from "../wrapper";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import {
   DEFINE,
   DEFINE_PURPLE,
@@ -17,120 +17,144 @@ import {
   TEST,
   TEST_PURPLE,
 } from "../../icons";
-import useIntersectionObserver from "../common/intersectionObserver";
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+
+import { Carousel } from "react-responsive-carousel";
+import Wrapper from "../wrapper";
+import { debounce } from "lodash";
 import { isMobileDevice } from "../../utils/utils";
+import useIntersectionObserver from "../common/intersectionObserver";
+import { useWindowSize } from "../common/screenSizeObserver";
 
-const ServicesPage2 = () => {
+const boxArr = [
+  {
+    icon: <EMPHATHISE className="servicepage2_Icon" />,
+    iconHover: <EMPHATHISE_PURPLE className="servicepage2_IconHover" />,
+    text: "Emphathise",
+    paraText:
+      "Emphathise is the first stage of design thinking process, where the focus is on understanding the needs, feelings, and experiences of the users or stakeholders.",
+  },
+  {
+    icon: <DEFINE className="servicepage2_Icon" />,
+    iconHover: <DEFINE_PURPLE className="servicepage2_IconHover" />,
+    text: "Define",
+    paraText:
+      "In the define stage, designers analyze and synthesize the information gathered during the emphatize stage.",
+  },
+  {
+    icon: <IDEATE className="servicepage2_Icon" />,
+    iconHover: <IDEATE_PURPLE className="servicepage2_IconHover" />,
+    text: "Ideate",
+    paraText:
+      "Brainstorming sessions, sketches, mind mapping, and other techniques, are used to encourage free thinking and explore different possibilities.",
+  },
+  {
+    icon: <PROTOTYPE className="servicepage2_Icon" />,
+    iconHover: <PROTOTYPE_PURPLE className="servicepage2_IconHover" />,
+    text: "Prototype",
+    paraText:
+      "The purpose of prototyping is to quickly test and interate on different solutions, allowing designers to gather feedback, learn and define their designs.",
+  },
+  {
+    icon: <IMPLEMENT className="servicepage2_Icon" />,
+    iconHover: <IMPLEMENT_PURPLE className="servicepage2_IconHover" />,
+    text: "Implement",
+    paraText:
+      "The implement stage is where design solution is put into action. It invloves executing the plans and strategies developed during the earlier stages.",
+  },
+  {
+    icon: <TEST className="servicepage2_Icon" />,
+    iconHover: <TEST_PURPLE className="servicepage2_IconHover" />,
+    text: "Test",
+    paraText:
+      "Testing helps identify strengths, weaknesses, and areas for improvement in the design, allowing designers to make informed refinements before moving forward.",
+  },
+  {
+    icon: <LAUNCH className="servicepage2_Icon" />,
+    iconHover: <LAUNCH_PURPLE className="servicepage2_Icon" />,
+    text: "Launch",
+    paraText:
+      "The launch stage marks the implementation or release of the final solution to the intended audience or market.",
+  },
+];
 
-  if(typeof document === "undefined") return<></>;
+const sliderCaptions = ["Understand", "Explore", "Materialise"];
 
-  const [_counter, _setCounter] = React.useState(0);
 
-  const boxArr = [
-    {
-      icon: <EMPHATHISE className="servicepage2_Icon" />,
-      iconHover: <EMPHATHISE_PURPLE className="servicepage2_IconHover" />,
-      text: "Emphathise",
-      paraText:
-        "Emphathise is the first stage of design thinking process, where the focus is on understanding the needs, feelings, and experiences of the users or stakeholders.",
-    },
-    {
-      icon: <DEFINE className="servicepage2_Icon" />,
-      iconHover: <DEFINE_PURPLE className="servicepage2_IconHover" />,
-      text: "Define",
-      paraText:
-        "In the define stage, designers analyze and synthesize the information gathered during the emphatize stage.",
-    },
-    {
-      icon: <IDEATE className="servicepage2_Icon" />,
-      iconHover: <IDEATE_PURPLE className="servicepage2_IconHover" />,
-      text: "Ideate",
-      paraText:
-        "Brainstorming sessions, sketches, mind mapping, and other techniques, are used to encourage free thinking and explore different possibilities.",
-    },
-    {
-      icon: <PROTOTYPE className="servicepage2_Icon" />,
-      iconHover: <PROTOTYPE_PURPLE className="servicepage2_IconHover" />,
-      text: "Prototype",
-      paraText:
-        "The purpose of prototyping is to quickly test and interate on different solutions, allowing designers to gather feedback, learn and define their designs.",
-    },
-    {
-      icon: <IMPLEMENT className="servicepage2_Icon" />,
-      iconHover: <IMPLEMENT_PURPLE className="servicepage2_IconHover" />,
-      text: "Implement",
-      paraText:
-        "The implement stage is where design solution is put into action. It invloves executing the plans and strategies developed during the earlier stages.",
-    },
-    {
-      icon: <TEST className="servicepage2_Icon" />,
-      iconHover: <TEST_PURPLE className="servicepage2_IconHover" />,
-      text: "Test",
-      paraText:
-        "Testing helps identify strengths, weaknesses, and areas for improvement in the design, allowing designers to make informed refinements before moving forward.",
-    },
-    {
-      icon: <LAUNCH className="servicepage2_Icon" />,
-      iconHover: <LAUNCH_PURPLE className="servicepage2_Icon" />,
-      text: "Launch",
-      paraText:
-        "The launch stage marks the implementation or release of the final solution to the intended audience or market.",
-    },
-  ];
-
-  const wrapperRef = useRef<any>();
-  const fromTop = useRef<boolean>(true)
-
+const Servicespage2 = () => {
+  
+  const _counter = useRef({
+    current: 0,
+    previous: 0,
+  });
+  const [currentActiveItem, setCurrentActiveItem] = useState(1)
   const triggerRef = useRef<any>();
+  const fromTop = useRef<boolean>(true)
   const activatingFromOutside = useRef(true)
-
-  const dataRef = useIntersectionObserver(triggerRef, {
+  const disableRef = useRef(false)
+  const [width, _] = useWindowSize();
+  
+  let dataRef = useIntersectionObserver(triggerRef, {
     threshold: 0.4,
     freezeOnceVisible: false,
-    root: document.getElementById('parallaxLayoutServices')
+    root: !(typeof document === "undefined") ? document.getElementById('parallaxLayoutServices') : null
   });
-
-  const sliderCaptions = ["Understand", "Explore", "Materialise"];
-
-  const scrollHorizontally = (evt: any) => {
+  
+  const scrollHorizontally = debounce((evt: any) => {
+    console.log("we are here", fromTop.current, disableRef.current)
     const scroller = document.getElementById('parallaxLayoutServices')
     if(!scroller) return;
     scroller.style.overflow = 'hidden'
     evt.preventDefault();
+    if(disableRef.current && ![6, 0].includes(_counter.current.current)) return;
+    console.log(_counter);
     
-    wrapperRef.current.scrollLeft += (evt.deltaY * 0.1);
-
-    const horizontalScrollPercentage = ((wrapperRef.current.scrollLeft + wrapperRef.current.clientWidth) / wrapperRef.current.scrollWidth) * 100
-
-    if(horizontalScrollPercentage >= 0 && horizontalScrollPercentage < 68){
-      _setCounter(0);
-    } else if(horizontalScrollPercentage >= 68 && horizontalScrollPercentage < 90){
-      _setCounter(1);
-    }else if(horizontalScrollPercentage >= 90 && horizontalScrollPercentage <= 100){
-      _setCounter(2);
-    }else{
-      _setCounter(0);
+    if(_counter.current.previous !== _counter.current.current){
+      disableRef.current = true
     }
 
-    if(((wrapperRef.current.scrollLeft + wrapperRef.current.clientWidth) - wrapperRef.current.scrollWidth) === 0){
+    console.log(evt.deltaY);
+    
+    
+    if(evt.deltaY > 0){
+      console.log('got here');
+      
+      document.getElementById('move_right')?.click()
+      if(_counter.current.current === 6){
+        _counter.current = {current: 7, previous: 6}
+      }
+    }else{
+      console.log('got here as well');
+      document.getElementById('move_left')?.click()
+      if(_counter.current.current === 0){
+        _counter.current = {current: -1, previous: 0}
+      }
+    }
+
+    if(_counter.current.current === 7){
+      console.log('here');
+      
       scroller.style.overflowY = 'scroll'
       activatingFromOutside.current = true
-      wrapperRef.current.scrollLeft = wrapperRef.current.scrollWidth - wrapperRef.current.clientWidth
-      wrapperRef.current.style.overflow = "hidden"
       fromTop.current = false
-      scroller?.removeEventListener('wheel', scrollHorizontally)
-    }else if(wrapperRef.current.scrollLeft === 0){
+      _counter.current = {previous: 6, current: 6}
+      scroller?.removeEventListener('wheel', debouceHandleHorizontalScroll)
+    }else if(_counter.current.current === -1){
+      console.log('there');
+      
       scroller.style.overflowY = 'scroll'
       scroller.scrollTo({
         top: 0,
         behavior: 'smooth'
       })
       activatingFromOutside.current = true
-      wrapperRef.current.style.overflow = "hidden"
       fromTop.current = true
-      scroller?.removeEventListener('wheel', scrollHorizontally)
+      _counter.current = {previous: 0, current: 0}
+      scroller?.removeEventListener('wheel', debouceHandleHorizontalScroll)
     }
-  }
+  }, 40)
+
+  const debouceHandleHorizontalScroll = useCallback((evt: Event) => scrollHorizontally(evt), []);
 
   const handleScrollMainPage = () => {
     
@@ -138,30 +162,25 @@ const ServicesPage2 = () => {
     const processCarouselContainer = document.getElementById('servicespage2_parentContainer')
   
     if(!scroller || !processCarouselContainer) return;
-    activatingFromOutside.current = true
+    console.log(Math.floor(processCarouselContainer.getBoundingClientRect().top));
     
-    if(Math.floor(processCarouselContainer.getBoundingClientRect().top) > 90 && Math.floor(processCarouselContainer.getBoundingClientRect().top) < 96){
+    if([90, 91].includes(Math.floor(processCarouselContainer.getBoundingClientRect().top))){
       scroller.style.overflow = 'hidden'
-      scroller.addEventListener("wheel", scrollHorizontally)
+      scroller.addEventListener("wheel", debouceHandleHorizontalScroll)
     }
-    
   }
+
 
   useEffect(() => {
     const scroller = document.getElementById('parallaxLayoutServices')
     const processCarouselContainer = document.getElementById('servicespage2_parentContainer')
+    console.log('scrolling');
     
-    if(dataRef && dataRef?.isIntersecting && scroller && processCarouselContainer && activatingFromOutside.current){
+    if(dataRef && dataRef?.isIntersecting && scroller && processCarouselContainer){
       scroller.scrollTo({
-        top: (scroller.scrollTop + processCarouselContainer.getBoundingClientRect().top) - 94,
+        top: (scroller.scrollTop + processCarouselContainer.getBoundingClientRect().top) - 91,
         behavior: 'smooth'
       })
-      wrapperRef.current.style.overflow = 'scroll hidden'
-
-      if(fromTop.current){
-        wrapperRef.current.scrollLeft = 2
-      }
-      activatingFromOutside.current = false
     }
 
     
@@ -169,8 +188,18 @@ const ServicesPage2 = () => {
       scroller?.addEventListener('scroll', handleScrollMainPage)
     }
 
-  }, [dataRef?.intersectionRatio])
+  }, [dataRef?.isIntersecting])
 
+  const setPercentage = () => {
+    if(width > 1200){
+      return 33
+    }else if(width > 600 && width <= 1200){
+      return 50
+    }else{
+      return 100
+    }
+  }
+  
   return (
     <Wrapper id="servicespage2_parentContainer">
       <div className="servicespage2_parentContainer" ref={triggerRef}>
@@ -186,8 +215,39 @@ const ServicesPage2 = () => {
             </p>
           </div>
         </div>
-        <div ref={wrapperRef} className="servicespage2_wrapper mt-24">
-          <div className="servicespage2_slider">
+        <div className="servicespage2_wrapper mt-24">
+          <Carousel
+            infiniteLoop={false}
+            centerMode
+            centerSlidePercentage={setPercentage()}
+            showArrows
+            showIndicators={false}
+            dynamicHeight
+            stopOnHover
+            swipeable
+            showThumbs={false}
+            showStatus={false}
+            preventMovementUntilSwipeScrollTolerance={true}
+            swipeScrollTolerance={50}
+            renderArrowPrev={(onClickHandler, hasPrev, label) =>
+              hasPrev && (
+                  <button type="button" id="move_left" onClick={onClickHandler} title={label} />
+              )
+          }
+          renderArrowNext={(onClickHandler, hasNext, label) =>
+              hasNext && (
+                  <button type="button" id="move_right" onClick={onClickHandler} title={label} />
+              )
+          }
+          onChange={(index, _) => {
+            _counter.current = {previous: _counter.current.current, current: index}
+            if(index !== 0){
+              setCurrentActiveItem(index)
+            }
+            disableRef.current = false
+          }}
+          
+          >
             {boxArr.map(
               (
                 item: {
@@ -212,7 +272,7 @@ const ServicesPage2 = () => {
                 );
               }
             )}
-          </div>
+          </Carousel>
         </div>
         <div className="flex items-center">
           {sliderCaptions.map((slider: string, index: number) => {
@@ -220,14 +280,14 @@ const ServicesPage2 = () => {
               <div className="servicespage2_dotContainer flex flex-1 items-center gap-4 mt-8">
                 <span
                   className={`servicespage2_dotBorder ${
-                    _counter === index ? "servicespage2_selectedDotBorder" : ""
+                    Math.ceil(currentActiveItem / 2) - 1 === index ? "servicespage2_selectedDotBorder" : ""
                   }`}
                   role="button"
                   tabIndex={0}
                 >
                   <div
                     className={`servicespage2_dotContent ${
-                      _counter === index
+                      Math.ceil(currentActiveItem / 2) - 1 === index
                         ? "servicespage2_selectedDotContent"
                         : ""
                     }`}
@@ -235,7 +295,8 @@ const ServicesPage2 = () => {
                 </span>
                 <span
                   className={`${
-                    _counter === index ? "primary100" : "offGray"
+                    Math.ceil(currentActiveItem / 2) - 1 === index
+                    ? "primary100" : "offGray"
                   } servicespage2_dotText`}
                 >
                   {slider}
@@ -251,7 +312,7 @@ const ServicesPage2 = () => {
         </div>
       </div>
     </Wrapper>
-  );
-};
+  )
+}
 
-export default ServicesPage2;
+export default Servicespage2
