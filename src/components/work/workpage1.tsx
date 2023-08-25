06b7b4c856de616352b4a11ef628d629028940f9
rogdevil <sep2001aryan@yahoo.com>
2023-08-25 15:52:08 +0530

    New work page ready

--- src/components/work/workpage1.tsx
+++ src/components/work/workpage1.tsx
@@ -1,64 +1,62 @@
-import React from "react";
 import "./workpage1.scss";
-import Wrapper from "../wrapper";
+
+import React, { useEffect } from "react";
+
+import AuditTrust from "../../images/slider_dashboard.png";
 import BitFleq from "../../images/showcase_nft.png";
 import BnD from "../../images/simplify_work.png";
-import AuditTrust from "../../images/slider_dashboard.png";
-import { Link } from "gatsby";
-import { ARROW_RIGHT_SMALL } from "../../icons";
+import Logixa from "../../images/logixa.png";
+import StackableCard from "./stackablecard";
+import Wrapper from "../wrapper";
 
 const Workpage1 = () => {
+  if(typeof window === "undefined") return <></>;
+
   const portfolioContainer = [
     {
       img: BitFleq,
       caption: "WEB3",
       title: "BitFleqs",
       path: "/work/bitfleq",
+      description: "Revolutionizing Supply Chain Management with Blockchain Technology."
     },
     {
       img: BnD,
       caption: "INFRASTRUCTURE",
       title: "Build’n Deploy",
       path: "/work/bnd",
+      description: "Leveraging BuildnDeploy.co for Streamlined Software Development and Deployment."
     },
     {
       img: AuditTrust,
       caption: "CYBER SECURITY",
       title: "Audit Trust",
       path: "/work/audit_trust",
+      description: "One Platform to Manage Your Security Program and  Compliance Posture."
+    },
+    {
+      img: Logixa,
+      caption: "Transportation & Logistics",
+      title: "Logixa",
+      path: "/work",
+      description: "Revolutionizing Shipment Management Solutions for Enhanced Logistics and Seamless Supply Chain Operations"
     },
   ];
+
+  useEffect(() => {
+    const stack = document.getElementById('workpage1_stack')
+    if(!stack) return    
+    stack.style.paddingBottom = `${window.innerHeight - 400}px`
+  }, [window])
 
   return (
     <Wrapper>
       <div className="workpage1_parentContainer">
         <h4 className="workpage1_title">Portfolio</h4>
-        <div className="workpage1_cardsContainer">
-          {portfolioContainer.map(
-            (portfolio: Record<string, any>, index: number) => {
-              return (
-                <div key={index} className="workpage1_workBox">
-                  <Link to={portfolio.path}>
-                    <div className="workpage1_cardImg">
-                      <img src={portfolio.img} alt="cardImg" />
-                    </div>
-                  </Link>
-                  <div className="workpage1_contentWrapper">
-                    <div className="workpage1_content">
-                      <p className="workpage1_caption">{portfolio.caption}</p>
-                      <p className="workpage1_title">{portfolio.title}</p>
-                    </div>
-                    <div className="workpage1_gobtn">
-                      <Link to={portfolio.path} className="animate">
-                        <ARROW_RIGHT_SMALL className="workpage1_gobtnIcon" />
-                        <ARROW_RIGHT_SMALL className="workpage1_gobtnIcon" />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              );
-            }
-          )}
+        <div className="workpage1_stack" id="workpage1_stack">
+          {portfolioContainer.map((work, index) => {
+            return(<StackableCard work={work} index={index} key={index} />)
+          })}
         </div>
       </div>
     </Wrapper>
