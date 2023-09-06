b82366302c0b1f0d03b9d0a778af56b16c7b9f75
rogdevil <sep2001aryan@yahoo.com>
2023-09-06 13:44:57 +0530

    fixed career page stack scroll

--- src/components/career/careerCard.tsx
+++ src/components/career/careerCard.tsx
@@ -1,37 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
 import Button from "../button";
 import { iconMap } from "../../jobData";
-import jobDataJSON from "../../../job_data.json";
 import { navigate } from "gatsby";
 
-const CareerCard = () => {
+interface ICareerCard {
+  data: Record<string, string | string[]>;
+  dataKey: string;
+  index: number;
+}
 
-  const typedJobDataJSON = jobDataJSON as Record<
-    string,
-    Record<string, string | string[]>
-  >;
+const CareerCard = ({data, dataKey, index}: ICareerCard) => {
+
+  const SVG = iconMap[data.icon as string]
+  const triggerRef = useRef<HTMLDivElement>(null)
+  const TOP_THRESHOLD = 160
+  const BOTTOM_THRESHOLD = 200
+
+  const handleStackScroll = (e: Event) => {
+    const trigger = triggerRef.current as HTMLElement
+    const cardDistFromTop = trigger.getBoundingClientRect().top
+    if(index === 0){
+      console.log(cardDistFromTop);
+    }
+    
+    if(cardDistFromTop < BOTTOM_THRESHOLD){
+      const rotationPercentage = +(((BOTTOM_THRESHOLD - cardDistFromTop) / (BOTTOM_THRESHOLD - TOP_THRESHOLD)) * 100).toFixed(2)
+      if(rotationPercentage <= 100){
+        trigger.style.transform = index % 2 === 0 ? `rotate(${(4/100) * rotationPercentage}deg)` : `rotate(-${(2/100) * rotationPercentage}deg)`
+      }
+    }else{
+      trigger.style.transform = `rotate(0deg)`
+    }
+  }
 
   const handleViewDetails = (dataKey: string) => {
     navigate(`/career/${dataKey}`)
   }
 
+  useEffect(() => {
+    const scroller = document.getElementById('career_scroller')
+    if(!scroller) return
+    scroller.addEventListener('scroll', handleStackScroll)
+  }, [])
+
   return (
     <>
-      {Object.keys(jobDataJSON).map((dataKey: string, index: number) => {
-        const SVG = iconMap[typedJobDataJSON[dataKey].icon as string]
-        return(
-          <div className="careerPage3_card" key={index}>
-            <div className="icon_container">
-              <SVG className="careerCardIcon" />
-            </div>
-            <p className="jobPosition">{typedJobDataJSON[dataKey].position}</p>
-            <div className="otherInfo">
-              <p className="location">Remote</p>
-              <Button className="viewDetailButton" onClick={() => handleViewDetails(dataKey)} buttonText="VIEW DETAILS" />
-            </div>
-          </div>
-        )
-      })}
+      <div className="careerPage3_card" ref={triggerRef}>
+        <div className="icon_container">
+          <SVG className="careerCardIcon" />
+        </div>
+        <p className="jobPosition">{data.position}</p>
+        <div className="otherInfo">
+          <p className="location">Remote</p>
+          <Button className="viewDetailButton" onClick={() => handleViewDetails(dataKey)} buttonText="VIEW DETAILS" />
+        </div>
+      </div>
     </>
   );
 };
--- src/components/career/careerPage3.tsx
+++ src/components/career/careerPage3.tsx
@@ -1,178 +1,27 @@
-import React, { useEffect, useRef } from "react";
-import Wrapper from "../wrapper";
 import "./careerPage3.scss";
+
 import Button from "../button";
-import { navigate } from "gatsby";
-import useIntersectionObserver from "../common/intersectionObserver";
 import CareerCard from "./careerCard";
+import React from "react";
+import Wrapper from "../wrapper";
+import jobDataJSON from "../../../job_data.json";
+import { navigate } from "gatsby";
 
 const CareerPage3 = () => {
-  const elementsToStack = useRef(-1);
-  const activatingFromOutside = useRef(true);
-  const lockMainScroll = useRef(false);
-  const scrollLockingPos = 91;
-  const scrollingToTop = useRef(false);
-  const scrollingToBottom = useRef(false);
 
-  const triggerRef = useRef<any>();
-
-  const cardRef = useIntersectionObserver(triggerRef, {
-    threshold: 0.2,
-    freezeOnceVisible: false,
-    root:
-      typeof document !== "undefined"
-        ? document.getElementById("career_scroller")
-        : "",
-  });
+  const typedJobDataJSON = jobDataJSON as Record<
+    string,
+    Record<string, string | string[]>
+  >;
 
   const handleAboutUs = () => {
     navigate("/about/");
   };
 
-  const handleScrollCardView = () => {
-    const scroller = document.getElementById("career_scroller");
-    const careerPageCardView = document.getElementById("careerPage3_cardView");
-    const section2 = document.getElementById("careerPage2_parentContainer");
-
-    if (!scroller || !careerPageCardView || !section2) {
-      return;
-    }
-    if(!lockMainScroll.current){
-      return
-    }
-    activatingFromOutside.current = false;
-    if (
-      careerPageCardView?.scrollHeight - careerPageCardView?.scrollTop <=
-        1556 ||
-      (careerPageCardView?.scrollTop === 0 && elementsToStack.current <= 0)
-    ) {
-      if (
-        careerPageCardView?.scrollHeight - careerPageCardView?.scrollTop <=
-        1556
-      ) {
-        scrollingToBottom.current = true
-        scroller.style.overflow = "hidden scroll";
-        scroller.scrollTo({
-          top: 3336,
-          behavior: "smooth",
-        });
-        careerPageCardView.style.overflow = "hidden";
-      } else if (
-        careerPageCardView?.scrollTop === 0 &&
-        elementsToStack.current <= 0
-      ) {
-        scrollingToTop.current = true
-        scroller.style.overflow = "hidden scroll";
-        scroller.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-        careerPageCardView.style.overflow = "hidden";
-      }
-      activatingFromOutside.current = true
-      lockMainScroll.current = false;
-    }
-    const cardsArray = Array.from(
-      document.getElementsByClassName("careerPage3_card")
-    );
-    if (careerPageCardView) {
-      // logic to remove the fixed class
-      if (careerPageCardView.scrollTop === 0 && elementsToStack.current >= 0) {
-        cardsArray[elementsToStack.current].classList.remove("fixed");
-        if (elementsToStack.current === 0) {
-          careerPageCardView.style.paddingTop = `0px`;
-        }
-        if (elementsToStack.current > 0) {
-          careerPageCardView.scrollTop = 200;
-        }
-        elementsToStack.current = elementsToStack.current - 1;
-      }
-
-      // logic to increment the fixed elements
-      if (
-        Math.floor(careerPageCardView.scrollTop / 306) >= 1 &&
-        elementsToStack.current < cardsArray.length - 2
-      ) {
-        elementsToStack.current = elementsToStack.current + 1;
-      } else if (
-        careerPageCardView.scrollTop >= 5 &&
-        elementsToStack.current === -1
-      ) {
-        elementsToStack.current = elementsToStack.current + 1;
-      }
-
-      // logic to add the fixed class
-      for (let i = 0; i < cardsArray.length; i++) {
-        if (
-          i <= elementsToStack.current &&
-          !cardsArray[i].classList.contains("fixed") &&
-          elementsToStack.current < cardsArray.length - 1
-        ) {
-          cardsArray[i].classList.add("fixed");
-          careerPageCardView.style.paddingTop = `${332}px`;
-          careerPageCardView.scrollTop = 1;
-        }
-      }
-    }
-  };
-
-  const handleScrollMainPage = (evt: Event) => {
-    if (lockMainScroll.current) {
-      evt.preventDefault();
-    }
-    const scroller = document.getElementById("career_scroller");
-    const scrollTopContainer = document.getElementById(
-      "careerPage3_parentContainer"
-    );
-
-    if (!scroller || !scrollTopContainer) return;
-    if(Math.floor(scrollTopContainer.getBoundingClientRect().top) !== scrollLockingPos){
-      scrollingToBottom.current = false
-      scrollingToTop.current = false
-    }
-    
-    if (
-      Math.floor(scrollTopContainer.getBoundingClientRect().top) === scrollLockingPos && 
-      activatingFromOutside.current &&
-      !scrollingToTop.current && !scrollingToBottom.current
-    ) {
-      activatingFromOutside.current = false;
-      scroller.style.overflow = "hidden";
-      lockMainScroll.current = true;
-    }
-  };
-
-  useEffect(() => {
-    const scroller = document.getElementById("career_scroller");
-    const scrollTopContainer = document.getElementById(
-      "careerPage3_parentContainer"
-    );
-    const careerPageCardView = document.getElementById("careerPage3_cardView");
-
-    if (!scrollTopContainer || !scroller || !careerPageCardView) return;
-
-    if (cardRef && cardRef?.isIntersecting && activatingFromOutside.current) {
-      scroller.scrollTo({
-        top:
-          scroller.scrollTop +
-          scrollTopContainer.getBoundingClientRect().top -
-          scrollLockingPos,
-        behavior: "smooth",
-      });
-      careerPageCardView.style.overflow = "hidden scroll";
-      careerPageCardView.scrollTop = 1;
-    }
-
-    scroller?.addEventListener("scroll", handleScrollMainPage);
-
-    careerPageCardView?.addEventListener("scroll", handleScrollCardView);
-  }, [cardRef?.intersectionRatio]);
-
   return (
-    <Wrapper>
+    <Wrapper id="careerPage3">
       <div
         className="careerPage3_parentContainer"
-        ref={triggerRef}
         id="careerPage3_parentContainer"
       >
         <div className="careerPage3_headingWrapper">
@@ -183,7 +32,16 @@ const CareerPage3 = () => {
         </div>
         <div className="careerPage3_cardView_wrapper">
           <div className="careerPage3_cardView" id="careerPage3_cardView">
-            <CareerCard />
+            {Object.keys(jobDataJSON).map((dataKey: string, index: number) => {
+              return(
+                <CareerCard 
+                  data={typedJobDataJSON[dataKey]}
+                  dataKey={dataKey}
+                  key={dataKey} 
+                  index={index}
+                />
+              )
+            })}
           </div>
         </div>
       </div>
