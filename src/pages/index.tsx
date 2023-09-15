d86a19a324bbe5a5d0b0bcfc585b80964a3ee006
rogdevil <sep2001aryan@yahoo.com>
2023-09-15 20:49:24 +0530

    reverted cache policy for homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,17 +51,7 @@ const IndexPage: React.FC<PageProps> = () => {
   );
 };
 
-//BND-343
-
 export default IndexPage;
-
-// export async function getServerData() {
-//   return {
-//     headers: {
-//       'Cache-Control': 'public, max-age=10, s-maxage=60, stale-while-revalidate=240',
-//     }
-//   }
-// }
 
 export const Head = () => (
   <Seo />
