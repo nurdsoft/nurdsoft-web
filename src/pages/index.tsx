6320c2bf5cb25964d911c53666132b2058f9a17f
rogdevil <sep2001aryan@yahoo.com>
2023-09-15 20:50:29 +0530

    reverted changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -51,7 +51,17 @@ const IndexPage: React.FC<PageProps> = () => {
   );
 };
 
+//BND-343
+
 export default IndexPage;
+
+export async function getServerData() {
+  return {
+    headers: {
+      'Cache-Control': 'public, max-age=10, s-maxage=60, stale-while-revalidate=240',
+    }
+  }
+}
 
 export const Head = () => (
   <Seo />
