6010f0003eab1e58a9dfbf5716c69315ff0ff7d6
rogdevil <sep2001aryan@yahoo.com>
2023-09-20 15:41:35 +0530

    home page ready

--- src/components/homepage/herosection.tsx
+++ src/components/homepage/herosection.tsx
@@ -1,34 +1,18 @@
 import './herosection.scss'
 
-import React, { useEffect, useRef, useState } from 'react'
-
-import ClutchImg from "../../images/clutch.svg"
-import GlassdoorImg from "../../images/glassdoor.svg"
-import GoogleImg from "../../images/googleIcon.png"
+import Button from '../button'
 import HeroDesignIcon from '../../images/heroDesignIcon.svg'
 import HeroDevelopIcon from '../../images/heroDevelopIcon.svg'
 import HeroOptimizeIcon from '../../images/heroOptimizeIcon.svg'
 import HeroSettingsIcon from '../../images/heroSettingsIcon.svg'
 import HeroSyncIcon from '../../images/heroSyncIcon.svg'
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
 import Wrapper from '../wrapper'
 
 const HeroSection = () => {
 
   if(typeof window === 'undefined' || typeof document === 'undefined') return <></>
-  
-  const [pageBlur, setPageBlur] = useState(false)
-
-  // ref for fading child start
-  const intervalRef = useRef<NodeJS.Timer[]>([])
-  const timeoutRefOne = useRef<NodeJS.Timeout[]>([])
-  const timeoutRefTwo = useRef<NodeJS.Timeout[]>([])
-  // ref for fading child end
-
-  // ref for fading child start
-  const intervalGradRef = useRef<NodeJS.Timer[]>([])
-  const timeoutGradRefOne = useRef<NodeJS.Timeout[]>([])
-  const timeoutGradRefTwo = useRef<NodeJS.Timeout[]>([])
-  // ref for fading child end
 
   const fadingChild = [
     {
@@ -52,131 +36,47 @@ const HeroSection = () => {
       img: HeroDesignIcon
     },
   ]
-
-  // animation logic fro fading child start
-  const intervalAnimation = (child: Element, index: number) => {
-    const timeoutOne = setTimeout(() => {
-      if(!document.hidden){
-        child.classList.add('animate')
+  const FadingChildren = () => (
+    <div id='first'>
+      {
+        fadingChild.map(({text, img}, _) => (
+          <h1>
+            {text}
+            <span><img src={img} alt='HeroSettingsIcons' /></span>
+          </h1>
+        ))
       }
-      const timeoutTwo = setTimeout(() => {
-        if(!document.hidden){
-          child.classList.remove('animate')
-        }
-      }, 2000)
-      timeoutRefTwo.current[index] = timeoutTwo
-    }, index * 1000 * 1.5)
-    timeoutRefOne.current[index] = timeoutOne
-    return
-  }
+    </div>
+  )
 
-  useEffect(() => {
-    const fadingChild = Array.from(document.getElementsByClassName('fading-child')[0].children)
-    intervalRef.current.forEach((intervalId) => {
-      clearInterval(intervalId)
-    })
-    timeoutRefOne.current.forEach((timeoutId) => {
-      clearTimeout(timeoutId)
-    })
-    timeoutRefTwo.current.forEach((timeoutId) => {
-      clearTimeout(timeoutId)
-    })
-    fadingChild.forEach((child, index) => {
-      intervalAnimation(child, index)
-      const intervalId = setInterval(() => {
-        if(!document.hidden){
-          intervalAnimation(child, index)
-        }
-      }, fadingChild.length * 1000 * 1.5 )
-      intervalRef.current.push(intervalId)
-    })
-  }, [pageBlur])
-  // animation logic for fading child end
-
-  // animation logic for gradient animation text start
-  const intervalGardientUpdate = (child: Element, index: number) => {
-    const timeoutOne = setTimeout(() => {
-      if(!document.hidden){
-        child.classList.add('animate')
-      }
-      const timeoutTwo = setTimeout(() => {
-        if(!document.hidden){
-          child.classList.remove('animate')
-        }
-      }, 3000)
-      timeoutGradRefTwo.current[index] = timeoutTwo
-    }, index * 1000 * 3)
-    timeoutGradRefOne.current[index] = timeoutOne
-    return
-  }
-
-  useEffect(() => {
-    const lineOne = Array.from(document.getElementsByClassName('lineOne')[0].children) as HTMLSpanElement[]
-    intervalGradRef.current.forEach((intervalId) => {
-      clearInterval(intervalId)
-    })
-    timeoutGradRefOne.current.forEach((timeoutId) => {
-      clearTimeout(timeoutId)
-    })
-    timeoutGradRefTwo.current.forEach((timeoutId) => {
-      clearTimeout(timeoutId)
-    })
-    lineOne.forEach((child, index) => {
-      intervalGardientUpdate(child, index)
-      const intervalId = setInterval(() => {
-        if(!document.hidden){
-          intervalGardientUpdate(child, index)
-        }
-      }, lineOne.length * 1000 * 3 )
-      intervalGradRef.current.push(intervalId)
-    })
-  }, [pageBlur])
-  // animation logic for gradient animation text end
-
-  useEffect(() => {
-    if(!document.hidden && pageBlur){
-      setPageBlur(false)
-    }else if(document.hidden && !pageBlur){
-      setPageBlur(true)
-    }
-  }, [])
+  const handleScrollUp = () => {
+    const wraperHeight = document.getElementById('herosection_parentContainer')?.clientHeight || 0
+    document.getElementById('home_scroller')?.scrollTo({top: wraperHeight, behavior: 'smooth'})
+  } 
 
   return (
     <Wrapper id='herosection_parentContainer'>
       <div className="herosection_parentContainer">
         <div className='hero_text'>
           <h1 className='lineOne'>
-            <span>Software</span>
-            <span>engineering</span>
-            <span>studio</span>     
+            Bespoke Software <br/>
+            Development Studio    
           </h1>
           <div className='animated-line'>
             <h1>we</h1>
             <div className='fading-child'>
-              {
-                fadingChild.map(({text, img}, _) => (
-                  <h1>
-                    {text}
-                    <span><img src={img} alt='HeroSettingsIcons' /></span>
-                  </h1>
-                ))
-              }
+              <div className='child-wrapper' id='fading-child-wrapper'>
+                <FadingChildren />
+              </div>
             </div>
           </div>
-          <h1 className='lineThree'>stunning online experiences</h1>
-        </div>
-        <div className='rating-wrapper'>
-          <p>Take a look at our reviews</p>
-          <div className='rating-box-wrapper'>
-            <div className='rating-box'>
-              <img src={ClutchImg} alt='ClutchImg' />
+          <h1 className='lineThree'>Solutions that meet your unique needs and help you achieve your goals.</h1>
+          <Button buttonText='SCHEDULE CONSULTATION' />
+          <div onClick={handleScrollUp} className='scroll-bottom'>
+            <div className='icon'>
+              <StaticImage placeholder="blurred" src="../../images/scrollDown.svg" className='scrollArrow' alt='scrollArrow' />
             </div>
-            <div className='rating-box'>
-              <img src={GoogleImg} alt='GoogleImg' />
-            </div>
-            <div className='rating-box'>
-              <img src={GlassdoorImg} alt='GlassdoorImg' />
-            </div>
+            <p>SCROLL DOWN</p>
           </div>
         </div>
       </div>
