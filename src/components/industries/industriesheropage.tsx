3c2456e371ecece51bb0784548ba99ab0ce9d9ce
rogdevil <sep2001aryan@yahoo.com>
2023-09-25 11:59:38 +0530

    hero animation for indistry page ready

---
+++ src/components/industries/industriesheropage.tsx
@@ -0,0 +1,80 @@
+import './industriesheropage.scss'
+
+import React from 'react'
+import Slider from 'react-slick'
+import Wrapper from '../wrapper'
+
+const IndustriesHeroPage = () => {
+  const data = [
+    "Healthcare",
+    "Media & Entertainment",
+    "Sports",
+    "Logistics",
+    "Fintech",
+    "Retail",
+    "Gaming",
+    "Hospitality & Leisure",
+    "Real Estate"
+  ]
+  return (
+    <Wrapper id="industryHeroPage_parentContainer">
+      <div className='industryHeroPage_parentContainer'>
+        <h1 className='heroText'>
+          Software <span className='primary100'>solutions </span> <br/> for every sector
+        </h1>
+        <div className='ribbonWrapper'>
+          <div className='ribbon'>
+            <div>
+              <div className='carouselRibbon'>
+                {
+                  data.map((industry, _) => (
+                    <div className='bulletPoint'>
+                      <div className='circle'></div>
+                      <div className='text'>{industry}</div>
+                    </div>
+                  ))
+                }
+              </div>
+              <div className='carouselRibbon'>
+                {
+                  data.map((industry, _) => (
+                    <div className='bulletPoint'>
+                      <div className='circle'></div>
+                      <div className='text'>{industry}</div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+          <div className='ribbon transparent'>
+            <div className='transparent'>
+              <div className='carouselRibbon'>
+                {
+                  data.map((industry, _) => (
+                    <div className='bulletPoint'>
+                      <div className='circle'></div>
+                      <div className='text'>{industry}</div>
+                    </div>
+                  ))
+                }
+              </div>
+              <div className='carouselRibbon'>
+                {
+                  data.map((industry, _) => (
+                    <div className='bulletPoint'>
+                      <div className='circle'></div>
+                      <div className='text'>{industry}</div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  )
+}
+
+export default IndustriesHeroPage
